8c0ec50636dc3305369718ae846e41a0f9ace2ed
Rizin Khader <110518497+httprizin@users.noreply.github.com>
2022-10-09 09:04:36 +0000

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,13 @@
+function bclick(val){
+    document.getElementById("screen").value=document.getElementById("screen").value+=val
+} 
+
+function bclear(){
+    document.getElementById("screen").value=" "
+}
+
+function eclick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById('screen').value=result 
+}
